3debd1b00c9594ed2db5dddd42889cfcf97373db
samrat neupane <samrat.neupane17013@gmail.com>
2023-07-13 10:42:52 +0545

    layout completed

---
+++ pages/Components/Button.tsx
@@ -0,0 +1,16 @@
+interface ButtonProps{
+    label:string;
+    secondary?:boolean;
+    fullWidth: boolean;
+    large?:boolean;
+    onClick:() =>void;
+    disabled?: boolean;
+    outline?:boolean;
+}
+
+
+const Button= () =>{
+    return (<div></div>);
+}
+
+export default Button;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-
+import Header from "./Components/Header/Header"
 export default function Home() {
   return (
-   <div className=" text-white">
-    hellow word
-   </div>
+   <>
+    <Header label='Home'/>
+   
+   </>
   )
 }
