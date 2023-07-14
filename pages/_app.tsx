30aa0b2795b6ca67dac88d2bca60e673e6860a70
samrat neupane <samrat.neupane17013@gmail.com>
2023-07-14 10:00:08 +0545

    Second day

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,12 @@ import Layout from './Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <>
+  
   <Layout>
     
   <Component {...pageProps} />
-  </Layout>)
+  </Layout>
+  </>
+  )
 }
